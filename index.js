a41125c7422bf86086bba64345ca04e903094202
Tim Mendoza <timmydoza@gmail.com>
2016-07-10 22:24:57 -0700

    finish tests

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var fs = require('fs');
 var resolve = require('path').resolve;
 
-module.exports = function(directory) {
+module.exports = function(directory, callback) {
+  fs.stat(resolve(directory), function(err, stat) {
+    if (err) {
+      callback(false);
+    }
+    callback(stat.isDirectory());
+  });
+};
+
+module.exports.sync = function(directory) {
   try {
     return fs.statSync(resolve(directory)).isDirectory();
   } catch (e) {
--- test/directory-exists-test.js
+++ test/directory-exists-test.js
@@ -4,16 +4,41 @@ var expect = chai.expect;
 var directoryExists = require(__dirname + '/../index.js');
 
 describe('The directoryExists function', function() {
+  it('return true if a directory exists and is a directory', function(done) {
+    var result = directoryExists(__dirname, function(result) {
+    expect(result).to.eql(true);
+    done();
+    });
+  });
 
-  var cwd = process.cwd();
+  it('should return false if path does not exist', function(done) {
+    var result = directoryExists(__dirname + '/fakeDirectory', function(result) {
+    expect(result).to.eql(false);
+    done();
+    });
+  });
 
-  it('return true if a directory exists', function() {
-    var result = directoryExists(cwd);
+  it('should return false if path is a file', function(done) {
+    var result = directoryExists(__dirname + '/directory-exists-test.js', function(result) {
+    expect(result).to.eql(false);
+    done();
+    });
+  });
+});
+
+describe('The directoryExists.sync function', function() {
+  it('return true if a directory exists and is a directory', function() {
+    var result = directoryExists.sync(__dirname);
     expect(result).to.eql(true);
   });
 
-  it('should return false if a directory does not exist', function() {
-    var result = directoryExists(cwd + '/fakrDir');
+  it('should return false if path does not exist', function() {
+    var result = directoryExists.sync(__dirname + '/fakeDirectory');
+    expect(result).to.eql(false);
+  });
+
+  it('should return false if path is a file', function() {
+    var result = directoryExists.sync(__dirname + '/directory-exists-test.js');
     expect(result).to.eql(false);
   });
 });
