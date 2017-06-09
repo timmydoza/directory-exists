69b8d6ac36456950ef3d5ada1e909d7491afa21b
Tim Mendoza <timmydoza@gmail.com>
2017-06-08 21:12:04 -0400

    update tests

--- test/directory-exists-test.js
+++ test/directory-exists-test.js
@@ -7,21 +7,21 @@ describe('The directoryExists function', function() {
   it('return true if a directory exists and is a directory', function(done) {
     var result = directoryExists(__dirname, function(result) {
     expect(result).to.eql(true);
-    done();
+    setImmediate(done);
     });
   });
 
   it('should return false if path does not exist', function(done) {
     var result = directoryExists(__dirname + '/fakeDirectory', function(result) {
     expect(result).to.eql(false);
-    done();
+    setImmediate(done);
     });
   });
 
   it('should return false if path is a file', function(done) {
     var result = directoryExists(__dirname + '/directory-exists-test.js', function(result) {
     expect(result).to.eql(false);
-    done();
+    setImmediate(done);
     });
   });
 });
